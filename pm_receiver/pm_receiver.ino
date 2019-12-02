9541baaa3557a27715b8195f31472a1bf803971e
Sky <mirrorcoloured@hotmail.com>
2019-12-01 18:10:39 -0700

    working demo

--- pm_receiver/pm_receiver.ino
+++ pm_receiver/pm_receiver.ino
@@ -1,24 +1,61 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
+#include <Servo.h>
 
-RH_ASK driver(2000, 2, 10, 5, false);
+#define TxPin 10
+#define RxPin 10
+#define PowerPin 16
+#define ServoPin 9
+
+RH_ASK driver(2000, RxPin, TxPin, PowerPin, false);
+Servo servo;
+int servo_position = 90;
+int last_servo_position = 0;
 
 void setup()
 {
-    Serial.begin(9600);  // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  Serial.begin(9600);  // Debugging only
+  Serial.println("Initializing");
+  
+  servo.attach(9);
+//  set_servo_position(90);
+  if (!driver.init())
+    Serial.println("init failed");
 }
 
-void loop()
-{
-    uint8_t buf[12];
-    uint8_t buflen = sizeof(buf);
-    if (driver.recv(buf, &buflen)) // Non-blocking
-    {
-      int i;
-      // Message with a good checksum received, dump it.
-      Serial.print("Message: ");
-      Serial.println((char*)buf);         
+void loop() {
+  uint8_t buf[50];
+  uint8_t buflen = sizeof(buf);
+  memset(buf, 0, buflen);
+  if (driver.recv(buf, &buflen)) {
+    Serial.print("Message: ");
+    Serial.println((char*)buf);
+    int servo_adjust = String((char*)buf).toInt();
+    switch (servo_adjust) {
+      case 1:
+        servo_position -= 5;
+        break;
+      case 2:
+        servo_position += 5;
+        break;
+      case 3:
+        servo_position = 90;
+        break;
     }
+  }
+//  Serial.print("Servo position: ");
+//  Serial.println(servo_position);
+  if (last_servo_position != servo_position) {
+    set_servo_position(servo_position);
+  }
+}
+
+void set_servo_position(int position) {
+  Serial.print("Setting servo position: ");
+  Serial.println(position);
+  if (position > 0 && position <= 180) {
+    servo.write(position);
+    last_servo_position = position;
+  }
+  delay(15);
 }
--- pm_transmitter/pm_transmitter.ino
+++ pm_transmitter/pm_transmitter.ino
@@ -1,21 +1,79 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
-RH_ASK driver(2000, 2, 10, 5, false);
+#define TxPin 10
+#define RxPin 10
+#define PowerPin 16
+
+#define LeftPin 4
+#define RightPin 3
+#define MiddlePin 2
+
+const unsigned int MAX_INPUT = 50;
+
+RH_ASK driver(2000, RxPin, TxPin, PowerPin, false);
 
 void setup()
 {
-    Serial.begin(9600);    // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  Serial.begin(9600);
+  Serial.println("Initializing");
+  if (!driver.init())
+     Serial.println("init failed");
 }
 
-void loop()
-{
-    //Serial.println(driver._txPin);
-    const char *msg = "Hello World!";
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    Serial.println("sent");
-    delay(1000);
+int pos = 0;
+
+void loop() {
+  // Send messages through serial input
+  while (Serial.available () > 0) {
+    processIncomingByte (Serial.read ());
+  }
+  
+  // Test buttons
+  if (digitalRead(LeftPin)) {
+    Serial.println("left");
+    transmit_data("1");
+  }
+  if (digitalRead(RightPin)) {
+    Serial.println("right");
+    transmit_data("2");
+  }
+  if (digitalRead(MiddlePin)) {
+    Serial.println("middle");
+    transmit_data("3");
+  }
+  delay(100);
+}
+  
+void processIncomingByte (const byte inByte) {
+  static char input_line [MAX_INPUT];
+  static unsigned int input_pos = 0;
+
+  switch (inByte) {
+    case '\n':   // end of text
+      input_line [input_pos] = 0;  // terminating null byte
+      process_data (input_line);
+      input_pos = 0;  
+      break;
+
+    case '\r':
+      break;
+
+    default:
+      // keep adding if not full ... allow for terminating null byte
+      if (input_pos < (MAX_INPUT - 1))
+        input_line [input_pos++] = inByte;
+      break;
+
+  }
+}
+
+void process_data (const char * data) {
+  transmit_data(data);
+}
+
+void transmit_data(const char * data) {
+  driver.send((uint8_t *)data, strlen(data));
+  Serial.print("sent ");
+  Serial.println(data);
 }
