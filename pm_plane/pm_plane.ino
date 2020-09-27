7597185d6381086d2d742960b0058dedc000496b
MatAff <mathijsaffourtit@gmail.com>
2020-09-26 20:38:58 -0600

    working on adding journey and segment concepts

--- pm_plane/pm_plane.ino
+++ pm_plane/pm_plane.ino
@@ -1,33 +1,33 @@
 /* HOW TO RUN
- * add additional boards managers to preferences
- * https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
- * in board manager search for SparkFun and add board manager for pro micro
- * set board
- * set processor
- * set port
- * upload
- */
+   add additional boards managers to preferences
+   https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+   in board manager search for SparkFun and add board manager for pro micro
+   set board
+   set processor
+   set port
+   upload
+*/
 
- /* REFERENCE
-  * pins: https://cdn.sparkfun.com/assets/9/c/3/c/4/523a1765757b7f5c6e8b4567.png
-  */
+/* REFERENCE
+   pins: https://cdn.sparkfun.com/assets/9/c/3/c/4/523a1765757b7f5c6e8b4567.png
+*/
 
 /* DESIGN SPEC
- * adjust control surfaces to maintain roll target (defaults to 0.0)
- * adjust tail to maintain pitch, reducte pitch when prop not running
- * run motor at start kill after set time
- * 
- */
+   adjust control surfaces to maintain roll target (defaults to 0.0)
+   adjust tail to maintain pitch, reducte pitch when prop not running
+   run motor at start kill after set time
 
- /* TODO
-  * update code to adhere to style guide
-  * include scale a global nature into variable names
-  * update scale function to rescale from any scale
-  * write tests for functions
-  * investigate options for strucs/classes
-  * add tail control based on pitch
-  * figure out why pitch > -20.0 work for cutting motor on decend
-  */
+*/
+
+/* TODO
+   update code to adhere to style guide
+   include scale a global nature into variable names
+   update scale function to rescale from any scale
+   write tests for functions
+   investigate options for strucs/classes
+   add tail control based on pitch
+   figure out why pitch > -20.0 work for cutting motor on decend
+*/
 
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
@@ -43,7 +43,41 @@
 #define ServoPinR 6
 #define ServoPinL 8
 #define ServoPinTail 4
-#define EscPin 9 
+#define EscPin 9
+
+// class to display fps at regular interval
+class FPS {
+  private:
+    int count = 0;
+    float fps = 0.0;
+    long lastUpdateTime = micros();
+    long SECOND = 1000000;
+  public:
+    get_fps() {
+      this->count += 1;
+      long currentTime = micros();
+      long deltaTime = currentTime - lastUpdateTime;
+
+      // calculate fps when sufficient time has passed
+      if (deltaTime > this->SECOND) {
+        this->fps = count / deltaTime * this->SECOND;
+        this->count = 0;
+        this->lastUpdateTime = currentTime;
+        Serial.print("fps: ");
+        Serial.println(this->fps);
+      } else {
+        return this->fps;
+      }
+    }
+};
+
+struct Segment {
+  String name;
+  float durationSecs;
+  float targetRoll;
+  float targetPitch;
+  float propSpeed;
+};
 
 // mode
 int mode = 3; // refers to button D3
@@ -70,7 +104,7 @@ float escLast = -1.0;
 int escStepSize = 0.01; // in unit
 
 // prop control
-unsigned long SECOND = 1000000; 
+unsigned long SECOND = 1000000;
 unsigned long escEndTime = micros() - (0 * SECOND);  // initialize esc end time as one second ago
 float escDurSecs = 5.0;
 float propSpeed = 0.75; // close to max
@@ -84,7 +118,7 @@ RH_ASK driver(2000, RxPin, TxPin, PowerPin, false);
 
 // IMU globals
 double roll = 0;
-double pitch = 0; 
+double pitch = 0;
 uint16_t BNO055_SAMPLERATE_DELAY_MS = 10;  // how often to read data from the board
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
@@ -92,41 +126,27 @@ uint16_t BNO055_SAMPLERATE_DELAY_MS = 10;  // how often to read data from the bo
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);  // TODO: unused, determine relevance
 unsigned long startTime = micros();
 
-class FPS {
-  private:
-    int count = 0;
-    float fps = 0.0;
-    long lastUpdateTime = micros();
-  public:
-    get_fps() {
-      this->count += 1;
-      long currentTime = micros();
-      long deltaTime = currentTime - lastUpdateTime;
-      if (deltaTime > 1000000) {        
-        this->fps = count / deltaTime * 1000000;
-        this->count = 0;
-        this->lastUpdateTime = currentTime;
-      } else {
-        return this->fps;
-      }
-    }
-};
+// fps
+FPS fps;
 
-float scale_unit(float val, float minVal, float maxVal, bool limit, bool invert) 
+// journey
+//Segment journey[] = {Segment(String("takeoff"), 3.0, 0.0, 0.0, 0.75)}; // TODO: get this working
+
+float scale_unit(float val, float minVal, float maxVal, bool limit, bool invert)
 {
   /* scale val from unit range [-1, 1] to [minVal, maxVal] */
-  
+
   val = (val + 1.0) / 2.0 * (maxVal - minVal) + minVal;
 
-  if (limit) { 
+  if (limit) {
     val = min(max(val, minVal), maxVal);
   }
-  
+
   if (invert) {
     val = maxVal - val + minVal;
   }
 
-  return val; 
+  return val;
 }
 
 float limit(float val, float minVal, float maxVal)
@@ -134,24 +154,24 @@ float limit(float val, float minVal, float maxVal)
   return min(max(val, minVal), maxVal);
 }
 
-float set_servo(Servo servo, float pos, bool invert=false, bool wordy=false) 
+float set_servo(Servo servo, float pos, bool invert = false, bool wordy = false)
 {
   float posScaled = scale_unit(pos, servoMin, servoMax, true, invert);
-  
+
   if (wordy) {
     Serial.print("Setting servo position: ");
     Serial.println(posScaled);
   }
 
   servo.write(posScaled);
-  
+
   delay(15);  // TODO: move delay to overall loop
-  
+
   return pos;
 }
 
 // DEB - scales, writes and returns
-float set_esc(Servo esc, float pos, bool invert=false, bool wordy=false) {
+float set_esc(Servo esc, float pos, bool invert = false, bool wordy = false) {
 
   // scale
   float posScaled = scale_unit(pos, escMin, escMax, true, false);
@@ -178,14 +198,14 @@ float pitch_standardize(float pitch)
   return -pitch;
 }
 
-void input_roll_pitch(bool wordy=false) 
+void input_roll_pitch(bool wordy = false)
 {
-  // set global roll and pitch 
+  // set global roll and pitch
   sensors_event_t orientationData, linearAccelData;
 
   //  triggered after delay
   if ((micros() - startTime) > (BNO055_SAMPLERATE_DELAY_MS * 1000)) {
-    
+
     startTime = micros(); // reset timer
 
     // get orientation and standardize
@@ -203,7 +223,7 @@ void input_roll_pitch(bool wordy=false)
   }
 }
 
-int input_receiver() 
+int input_receiver()
 {
   // return int of message received or zero
   uint8_t buf[50];
@@ -212,7 +232,7 @@ int input_receiver()
   int sentCode = 0;
 
   if (driver.recv(buf, &buflen)) {
-    sentCode = String((char*)buf).toInt();  // update sent code    
+    sentCode = String((char*)buf).toInt();  // update sent code
     Serial.print("Message: ");
     Serial.println(sentCode);
   }
@@ -221,7 +241,7 @@ int input_receiver()
 
 }
 
-void control_roll_pitch(float rollTarget=0.0, float pitchTarget=0.0)
+void control_roll_pitch(float rollTarget = 0.0, float pitchTarget = 0.0)
 {
   float rollMax = 15.0;
   float rollDelta = rollTarget - roll;
@@ -242,8 +262,8 @@ bool control_prop(int sentCode)
 {
 
   // update end esc end time
-  if (sentCode==1) {
-    escEndTime = micros() + (escDurSecs * SECOND); 
+  if (sentCode == 1) {
+    escEndTime = micros() + (escDurSecs * SECOND);
     Serial.println("setting");
     Serial.println(escEndTime);
   }
@@ -261,11 +281,11 @@ bool control_prop(int sentCode)
 
 void act_servos()
 {
-  
+
   if (servoLastLeft != servoPosLeft) {
     servoLastLeft = set_servo(servoL, servoPosLeft, true, false);
   }
-  
+
   if (servoLastRight != servoPosRight) {
     servoLastRight = set_servo(servoR, servoPosRight, false, false);
   }
@@ -280,9 +300,9 @@ void act_servos()
 void act_esc() {
 
   if (escLast != escPos) {
-    
+
     // limit to steps size
-//    float escPosLimit = escLast + limit(escPos - escLast, escStepSize, -escStepSize);
+    //    float escPosLimit = escLast + limit(escPos - escLast, escStepSize, -escStepSize);
     float escPosLimit = escPos;
 
     Serial.print("escPosLimit: ");
@@ -305,33 +325,36 @@ void setup()
   set_servo(servoL, servoPosLeft);
   set_servo(servoR, servoPosRight);
   set_servo(servoR, servoPosTail);
-  
+
   // esc
   esc.attach(EscPin);
   set_servo(esc, escPos); // TODO: drop, this shouldn't do anything
   esc_calibrate();
-  
+
   // receiver
   if (!driver.init()) {
     Serial.println("init failed - receiver");
     while (1); // TODO: figure out what this does
   }
-  
+
   // imu
   if (!bno.begin()) {
     Serial.print("No BNO055 detected");
     while (1); // TODO: figure out what this does
   }
   delay(1000);
-  
+
   // esc
   esc_calibrate();
 
   // finalize
   Serial.println("setup complete");
 
-  // kick off run
-  escEndTime = micros() + (escDurSecs * SECOND); 
+  // set kill time
+  escEndTime = micros() + (escDurSecs * SECOND);
+
+  // fps
+  fps = FPS();
 }
 
 void esc_calibrate() {
@@ -340,7 +363,7 @@ void esc_calibrate() {
   delay(2000);
   Serial.println("esc - high");
   esc.writeMicroseconds(escMax);
-  delay(2000);  
+  delay(2000);
   Serial.println("esc - low");
   esc.writeMicroseconds(escMin);
   delay(2000);
@@ -357,10 +380,13 @@ void loop() {
   // receiver
   int sentCode = input_receiver();
 
+  // fps
+  fps.get_fps();
+
   /* control section */
 
   // decision logic
-  if (mode==3) {
+  if (mode == 3) {
 
     // prop
     control_prop(sentCode);
@@ -369,7 +395,7 @@ void loop() {
     if (escPos < 0.0) {
       pitchTarget = -15.0;
     }
-    
+
     // roll
     control_roll_pitch(rollTarget, pitchTarget);
 
@@ -428,7 +454,7 @@ void printEvent(sensors_event_t* event) {
 //
 //// round to zero if value is near zero
 //float stick_near_zero(float xy, float thresh = 0.05)
-//{  
+//{
 //  if (abs(xy - 0.0) < thresh) {
 //    return 0.0;
 //  } else {
@@ -441,3 +467,30 @@ void printEvent(sensors_event_t* event) {
 //  return max(x - 0.02, 0.0);
 //}
 //
+
+//class Segment {
+//  private:
+//    String name;
+//    float durationSecs;
+//    float targetRoll;
+//    float targetPitch;
+//    float propSpeed;
+//  public:
+//    Segment(String name, float durationSecs, float targetRoll, float targetPitch, float propSpeed) {
+//      this->name = name;
+//      this->durationSecs = durationSecs;
+//      this->targetRoll = targetRoll;
+//      this->targetPitch = targetPitch;
+//      this->propSpeed = propSpeed;
+//      init();
+//    }
+//
+//    void init() { }
+//
+//    String getName() { return this->name; }
+//    float getDurationSecs() { return this->durationSecs; }
+//    float getTargetRoll() { return this->targetRoll; }
+//    float getTargetPitch() { return this->targetPitch; }
+//    float getPropSpeed() { return this->propSpeed; }
+//
+//};
