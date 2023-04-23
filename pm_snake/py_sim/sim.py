aa5430afc8e740feef82b17dc57f2285567e31d3
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2023-04-23 09:38:22 -0600

    Sim including collision

--- pm_snake/py_sim/sim.py
+++ pm_snake/py_sim/sim.py
@@ -1,8 +1,17 @@
+# ! python3 -m pip install opencv-python
+
+import cv2
 import numpy as np
 
-SEGMENT_LENGTH = 100  # mm
-SEGMENT_WIDTH = 50  # mm
+DISPLAY_SHAPE = (480, 640, 3)
+GRAVITY = 10
+DELTA_TIME = 50
+MS = (1 / 1000)
+DEBUG = False
 
+
+def overlap(source, target):
+    return (source[1] > target[0]) & (source[0] < target[1])
 
 class Vector:
 
@@ -12,29 +21,37 @@ class Vector:
         self.P = [x, y]
 
     def length(self):
-        print(self.x**2 + self.y**2)
         return (self.x**2 + self.y**2)**0.5
-
-    def __eq__(self, vector):
-        return (self.x == vector.x) & (self.y == vector.y)
 
     def norm(self):
         length = self.length()
         return Vector(self.x / length, self.y / length)
+
+    def perp(self):
+        return Vector(self.y, -self.x)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def cross(self, vector):
+        return self.x * vector.y - self.y * vector.x;
+
+    def to_int_list(self):
+        return [int(self.x), int(self.y)]
+
+    def __repr__(self):
+        return str({'x': self.x, 'y': self.y})
 
     def __mul__(self, obj):
         if isinstance(obj, Vector):
             return Vector(self.x * obj.x, self.y * obj.y)
         return Vector(self.x * obj, self.y * obj)
 
-    def perp(self):
-        return Vector(self.y, -self.x)
+    def __truediv__(self, obj):
+        if isinstance(obj, Vector):
+            return Vector(self.x / obj.x, self.y / obj.y)
+        return Vector(self.x / obj, self.y / obj)
 
-    def __repr__(self):
-        return str({'x': self.x, 'y': self.y})
-
-    def cross(self, vector):
-        return self.x * vector.y - self.y * vector.x;
 
     def __sub__(self, vector):
         return Vector(self.x - vector.x, self.y - vector.y)
@@ -42,100 +59,169 @@ class Vector:
     def __add__(self, vector):
         return Vector(self.x + vector.x, self.y + vector.y)
 
+    def __pow__(self, e):
+        return Vector(self.x**e, self.y**e)
 
-class Shape:
-
-    def __init__(self, vertices):
-        self.vertices = vertices
-        self.centroid = self.get_centroid()
-
-    def get_centroid(self):
-        t = list(zip(*self.vertices))
-        return Vector(np.mean(t[0]), np.mean(t[1]))
-
-    def get_area(self):
-        """Compute total area and the center of mass."""
-        self.total_area = 0
-        self.center_mass = Vector()
-        for pos, vertice in enumerate(self.vertices):
-            next_vertice = self.vertices[pos + 1 - len(self.vertices)]
-            base_edge = Vector(*vertice) - Vector(*next_vertice)
-            center_edge = Vector(*next_vertice) - self.centroid
-            area = 0.5 * np.abs(base_edge.cross(center_edge))
-            vertices = [vertice, next_vertice, self.centroid.P]
-            t = list(zip(*vertices))
-            area_mass = Vector(np.mean(t[0]), np.mean(t[1]))
-            combined = self.total_area + area
-            self.center_mass = self.center_mass * (self.total_area / combined) + area_mass * (area / combined)
-            self.total_area = self.total_area + area
-        return self.total_area, self.center_mass
+    def __eq__(self, vector):
+        return (self.x == vector.x) & (self.y == vector.y)
 
 
 class Body:
 
-    def __init__(self):
-        self.position = Vector()
+    def __init__(self,position, mass, inertia, restitution, color, name):
+        self.position = position
         self.velocity = Vector()
         self.acceleration = Vector()
         self.force = Vector()
-        self.mass = 0
+        self.mass = mass
+        self.name = name
 
         self.rotation = 0
         self.angular_velocity = 0
         self.angular_acceleration = 0
         self.torque = 0
-        self.inertia = 0
+        self.inertia = inertia
 
         self.friction = 0
-        self.restitution = 0
-        self.shape = Shape()
+        self.restitution = restitution
+        self.color = color
 
     def update(self, delta_time):
 
+        self.force = self.force + Vector(0, GRAVITY)
+
         # Position
-        self.acceleration = self.force / self.mass
-        self.velocity = self.velocity + self.acceleration * delta_time
-        self.position = self.position + self.velocity * delta_time
+        self.acceleration = self.force * (1 / self.mass)
+        self.velocity = self.velocity + self.acceleration * delta_time * MS
+        self.position = self.position + self.velocity * delta_time * MS
 
         # Rotation
-        self.angular_acceleration = self.torque / self.inertia
-        self.angular_velocity = self.angular_acceleration * delta_time
-        self.rotation = self.angular_velocity * delta_time
+        self.angular_acceleration = self.torque * (1 / self.inertia)
+        self.angular_velocity = self.angular_acceleration * delta_time * MS
+        self.rotation = self.angular_velocity * delta_time * MS
 
         # Zero forces
-        self.force = Vector()
+        self.force = Vector(0, 0)
         self.torque = 0
+
+    def box_overlap(self, other):
+        if overlap([self.get_box()[0].x, self.get_box()[1].x],
+                   [other.get_box()[0].x, other.get_box()[1].x]):
+            if overlap([self.get_box()[0].y, self.get_box()[1].y],
+                       [other.get_box()[0].y, other.get_box()[1].y]):
+                return True
+            return False
+        return False
+
+class Circle(Body):
+
+    def __init__(self, position, mass, radius, inertia, restitution, color, name):
+        super().__init__(position, mass, inertia, restitution, color, name)
+        self.radius = radius
+
+    def get_box(self):
+        return [Vector(-self.radius + self.position.x,
+                       -self.radius  + self.position.y),
+                Vector(self.radius + self.position.x,
+                       self.radius + self.position.y)]
+
+    def add_to_frame(self, frame):
+        cv2.circle(frame, self.position.to_int_list(), self.radius,
+                   self.color, 1)
+
+    def collide(self, other):
+        delta = self.position - other.position
+        return (delta.x**2 + delta.y**2) < (self.radius + other.radius)**2
+
+
+def resolve_collision(source, target):
+    collision_normal = (source.position - target.position).norm()
+    relative_velocity = source.velocity - target.velocity
+    velocity_along_normal = relative_velocity.dot(collision_normal)
+    if velocity_along_normal > 0:
+        return
+    restitution = min(source.restitution, target.restitution)
+    impulse_scalar =  -(1 + restitution) * velocity_along_normal
+    impulse_scalar = impulse_scalar / (1 / source.mass + 1 / target.mass)
+    impulse = collision_normal * impulse_scalar
+    source.velocity = source.velocity + impulse * (1 / source.mass)
+    target.velocity = target.velocity - impulse * (1 / target.mass)
 
 
 class World:
 
     def __init__(self):
         self.bodies = []
+        self.time = 0
+
+    def get_frame(self):
+        frame = np.zeros(DISPLAY_SHAPE, np.uint8)
+        for body in self.bodies:
+            body.add_to_frame(frame)
+        return frame
 
     def update(self, delta_time):
         for body in self.bodies:
             body.update(delta_time)
+        self.time = self.time + delta_time
+        if DEBUG:
+            print(self.time)
 
-    def display(self):
-        pass  # TODO(maffourtit): implement
+    def detect_colistions(self):
+        colisions = []
+        for pos, source in enumerate(self.bodies[:-1]):
+            for target in self.bodies[pos+1:]:
+                if source.box_overlap(target):
+                    #   print('overlap')
+                    if source.collide(target):
+                        colisions.append([source, target])
+                        print('boom!', source.name, target.name)
+                        resolve_collision(source, target)
+
+    def handle_colisions(self):
+        self.detect_colistions()
+
+
+class Display:
+
+    def __init__(self, name):
+        self.name = name
+        cv2.namedWindow(self.name)
+
+    def imshow(self, frame):
+        cv2.imshow(self.name, frame)
+        key = cv2.waitKey(10)
+        if key > 0:
+            print(key)
+        return key
+
+    def close(self):
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
 
+    display = Display('Sim')
+
     # Initiate world
     world = World()
+    left = Circle(Vector(100, 205), 10, 15, 5, 0.1, (0, 0, 255), 'red')
+    left.velocity = Vector(10, 0)
+    world.bodies.append(left)
 
-    # Add a body
-    head = Body()
-    shape = Shape([
-        [0, 0],
-        [SEGMENT_LENGTH / 2, SEGMENT_WIDTH / 2],
-        [SEGMENT_LENGTH, 0],
-        [SEGMENT_LENGTH / 2, -SEGMENT_WIDTH / 2],
-    ])
-    head.shape = shape
+    right = Circle(Vector(200, 195), 10, 15, 5, 0.2, (0, 255, 0), 'green')
+    right.velocity = Vector(-10, 0)
+    world.bodies.append(right)
 
-vertices = [[1, 2], [3, 4], [5, 6]]
-t = list(zip(*vertices))
-t
-Vector(np.mean(t[0]), np.mean(t[1]))
+    middle = Circle(Vector(150, 150), 10, 15, 5, 0.2, (255, 0, 0), 'blue')
+    middle.velocity = Vector(0, 10)
+    world.bodies.append(middle)
+
+    # Main loop
+    key = 0
+    while key != 27:
+        key = display.imshow(world.get_frame())
+        world.update(DELTA_TIME)
+        world.handle_colisions()
+
+    display.close()
--- pm_snake/py_sim/sim_test.py
+++ pm_snake/py_sim/sim_test.py
@@ -1,7 +1,8 @@
 import unittest
 
+from sim import Circle
 from sim import Vector
-from sim import Shape
+from sim import overlap
 
 
 class TestVector(unittest.TestCase):
@@ -9,11 +10,14 @@ class TestVector(unittest.TestCase):
     def test_length(self):
         self.assertEqual(Vector(3, 4).length(), 5)
 
-    def test_equals(self):
-        self.assertEqual(Vector(1, 1), Vector(1, 1))
-
     def test_norm(self):
         self.assertEqual(Vector(0, 2).norm(), Vector(0, 1))
+
+    def test_dot(self):
+        self.assertEqual(Vector(1, 2).dot(Vector(3, 4)), 11)
+
+    def test_to_int_list(self):
+        self.assertEqual(Vector(1, 2).to_int_list(), [1, 2])
 
     def test_mult_vector(self):
         self.assertEqual(Vector(2, 3) * Vector(4, 5), Vector(8, 15))
@@ -30,13 +34,97 @@ class TestVector(unittest.TestCase):
     def test_add(self):
         self.assertEqual(Vector(4, 3) + Vector(2, 1), Vector(6, 4))
 
+    def test_pow(self):
+        self.assertEqual(Vector(2, 3)**2, Vector(4, 9))
 
-class TestShape(unittest.TestCase):
+    def test_equals(self):
+        self.assertEqual(Vector(1, 1), Vector(1, 1))
 
-    def test_centroid(self):
-        shape = Shape([[0, 0], [0, 4], [2, 4], [2, 0]])
-        self.assertEqual(shape.centroid, Vector(1, 2))
 
-    def test_get_area(self):
-        shape = Shape([[0, 1], [2, 0], [4, 1], [2, 2]])
-        self.assertEqual(shape.get_area(), (4, Vector(2, 1)))
+class TestCircle(unittest.TestCase):
+
+    def test_get_box(self):
+        circle = Circle(Vector(0, 0), 5, 5, 5, 0.2, (0, 0, 255))
+        self.assertEqual(circle.get_box(), [Vector(-5, -5), Vector(5, 5)])
+
+class TestOverall(unittest.TestCase):
+
+    def test_overlap_lower(self):
+        self.assertTrue(overlap([1, 4], [0, 3]))
+
+    def test_overlap_middle(self):
+        self.assertTrue(overlap([1, 4], [2, 3]))
+
+    def test_overlap_higher(self):
+        self.assertTrue(overlap([1, 4], [2, 5]))
+
+    def test_overlap_too_low(self):
+        self.assertFalse(overlap([1, 4], [0, 0]))
+
+    def test_overlap_too_high(self):
+        self.assertFalse(overlap([1, 4], [5, 5]))
+
+
+# SEGMENT_LENGTH = 100  # mm
+# SEGMENT_WIDTH = 50  # mm
+# POINT_TEMPLATE = [
+#     [0, 0],
+#     [SEGMENT_LENGTH / 2, SEGMENT_WIDTH / 2],
+#     [SEGMENT_LENGTH, 0],
+#     [SEGMENT_LENGTH / 2, -SEGMENT_WIDTH / 2],
+# ]
+
+# from sim import Polygon
+
+# class TestPolygon(unittest.TestCase):
+
+#     def test_get_box(self):
+#         polygon = Polygon([[0, 0], [0, 4], [2, 4], [2, 0]])
+#         self.assertEqual(polygon.get_box(), [Vector(0, 0), Vector(2, 4)])
+
+
+#     def test_centroid(self):
+#         polygon = Polygon([[0, 0], [0, 4], [2, 4], [2, 0]])
+#         self.assertEqual(polygon.centroid, Vector(1, 2))
+
+#     def test_get_area(self):
+#         polygon = Polygon([[0, 1], [2, 0], [4, 1], [2, 2]])
+#         self.assertEqual(polygon.get_area(), (4, Vector(2, 1)))
+
+
+# class Polygon(Shape):
+
+#     def __init__(self, vertices):
+#         self.vertices = vertices
+#         self.transpose_vertices = list(zip(*self.vertices))
+#         self.centroid = self.get_centroid()
+
+#     def get_box(self):
+#         return [Vector(np.min(self.transpose_vertices[0]),
+#                        np.min(self.transpose_vertices[1])),
+#                 Vector(np.max(self.transpose_vertices[0]),
+#                        np.max(self.transpose_vertices[1]))]
+
+#     def get_centroid(self):
+#         return Vector(np.mean(self.transpose_vertices[0]),
+#                       np.mean(self.transpose_vertices[1]))
+
+#     def get_area(self):
+#         """Compute total area and the center of mass."""
+#         self.total_area = 0
+#         self.center_mass = Vector()
+#         for pos, vertice in enumerate(self.vertices):
+#             next_vertice = self.vertices[pos + 1 - len(self.vertices)]
+#             base_edge = Vector(*vertice) - Vector(*next_vertice)
+#             center_edge = Vector(*next_vertice) - self.centroid
+#             area = 0.5 * np.abs(base_edge.cross(center_edge))
+#             vertices = [vertice, next_vertice, self.centroid.P]
+#             t = list(zip(*vertices))
+#             area_mass = Vector(np.mean(t[0]), np.mean(t[1]))
+#             combined = self.total_area + area
+#             self.center_mass = (self.center_mass * (self.total_area / combined) +
+#                 area_mass * (area / combined))
+#             self.total_area = self.total_area + area
+#         return self.total_area, self.center_mass
+
+# Body(Vector(segment_number * SEGMENT_LENGTH), POINT_TEMPLATE))
