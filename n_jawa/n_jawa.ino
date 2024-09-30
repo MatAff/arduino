6842970de4c729aa59400b8754afb94ad234c246
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2024-09-29 19:53:19 -0600

    Updates

--- n_jawa/n_jawa.ino
+++ n_jawa/n_jawa.ino
@@ -1,12 +1,14 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN 6
+#define LED_PIN_2 5
+// TODO: Add another pin
 #define BUTTON_PIN 3
 #define N 100
 #define LIGHTS_PER_LIGHT 7
 #define LIGHTS 8
 
-int brightness = 100;  // 0 through 100
+int brightness = 10;  // 0 through 100
 int t = 0;  // Wheel color
 int buttonState = 0;
 unsigned long lastDebounceTime = 0;
@@ -15,7 +17,15 @@ int pressed_value = 50;
 int pressed_threshold = 50;
 int mode = 0;  // Track mode
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N, LED_PIN, NEO_GRBW + NEO_KHZ800);
+int upper = 200;
+int lower = 20;
+int g = lower;
+int dg = 1;
+// Start rgb 	(255,223,0)
+// End rgb (255,100,0)
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(N, LED_PIN_2, NEO_GRB + NEO_KHZ800);
 
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -23,6 +33,7 @@ void setup() {
 #endif  
   Serial.begin(9600);
   strip.begin();  // Lights
+  strip_2.begin();  // Lights
   pinMode(BUTTON_PIN, INPUT);  // Button
 }
 
@@ -42,44 +53,50 @@ void loop() {
   // Respond to button
   if (buttonState == HIGH) {
     Serial.println("Button pressed");
-    mode = mode + 1 % 6;
+    int numberModes = 3;
+    mode = mode + 1 % numberModes;
   }
 
   switch (mode) {
-    case 0 : all(0, 255, 0); break;
-    case 1 : all(255, 0, 0); break;
-    case 2 : all(0, 0, 255); break;
-    case 3 : runWheel(); break;
-    case 4 : runWheelSame(); break;
-    case 5:
-      brightness = brightness - 10;
+    case 0:
+      g = g + dg;
+      all(255, g, 0);
+      // all(0, 0, 0);
+      if ((g < lower) || (g > upper)) { dg = dg * -1; }
+      break;
+    case 1:
+      brightness = brightness - 5;
       mode = 0;
       Serial.println(brightness);
     default : all(0, 255, 0); break;
-    if (brightness < 20) {
-      brightness = 100;
-      if (brightness < 5) { brightness = 100; }
-    }
   }
 
+  if (brightness < 5) { brightness = 100; }
+
   strip.show();  // Display
+  strip_2.show();  // Display
   t = (t + 5) % 400;  // Update wheel color
   buttonState = 0;  // Reset button
   delay(50);  // 20 fps
 }
 
 void all(int r, int g, int b) {
-  for (int i = 0; i < LIGHTS_PER_LIGHT * LIGHTS; i++) { setLight(i, r, g, b); }
+  for (int i = 0; i < 20; i++) { setLight(i, r, g, b); }
 }
 
 void setLight(int led, int r, int g, int b) {
   strip.setPixelColor(led, strip.Color(int(r * brightness / 100), int(g * brightness / 100), int(b * brightness / 100)));
+  strip_2.setPixelColor(led, strip_2.Color(int(r * brightness / 100), int(g * brightness / 100), int(b * brightness / 100)));
 }
 
 void runWheel() {
   for (int i = 0; i < LIGHTS; i++) {
     for (int j = 0; j < LIGHTS_PER_LIGHT; j++) {
       strip.setPixelColor(
+        i * LIGHTS_PER_LIGHT + j,
+        Wheel((t + (400 / LIGHTS * i)) % 400)
+      );
+            strip_2.setPixelColor(
         i * LIGHTS_PER_LIGHT + j,
         Wheel((t + (400 / LIGHTS * i)) % 400)
       );
